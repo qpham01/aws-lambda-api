b7b1745d18e2f1f83cc92581ae2b945341c3b9b3
Quoc Pham <quoc_pham@msn.com>
2021-05-06 18:36:26 -0500

    Added inline policy and route key.

--- example/api.tf
+++ example/api.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "lambda_iam" {
-  name = "lambda_iam"
+resource "aws_iam_role" "lambda-iam" {
+  name = "lambda-iam"
 
   assume_role_policy = <<EOF
 {
@@ -16,27 +16,48 @@ resource "aws_iam_role" "lambda_iam" {
   ]
 }
 EOF
-  managed_policy_arns = ["arn:aws:iam::115648203036:policy/service-role/AWSLambdaEdgeExecutionRole-d15dbb38-5efb-4b5b-84cb-1e421124d709"]
+}
+
+resource "aws_iam_policy" "log-all" {
+  name = "log-all"
+  description = "Log to all"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+            "logs:*"
+        ],
+        "Resource": "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "log-attach" {
+  role       = "${aws_iam_role.lambda-iam.name}"
+  policy_arn = "${aws_iam_policy.log-all.arn}"
 }
 
 resource "aws_lambda_function" "add-blog" {
   filename          = "build.zip"
   function_name     = "AddBlogAsync"
-  role              = aws_iam_role.lambda_iam.arn
+  role              = aws_iam_role.lambda-iam.arn
   handler           = "ExampleApi::ExampleApi.Functions::AddBlogAsync"
   runtime           = "dotnetcore3.1"
+  source_code_hash  = "${filebase64sha256("build.zip")}"
 }
 
 resource "aws_apigatewayv2_api" "example" {
   name          = var.http_api_name
   protocol_type = "HTTP"
   target        = aws_lambda_function.add-blog.arn
+  route_key     = "POST /addblog"
 }
-
-# resource "aws_apigatewayv2_stage" "example-dev" {
-#   api_id = aws_apigatewayv2_api.example.id
-#   name   = "example-dev"
-# }
 
 resource "aws_lambda_permission" "add-blog" {
 	action        = "lambda:InvokeFunction"
@@ -45,9 +66,3 @@ resource "aws_lambda_permission" "add-blog" {
 
 	source_arn = "${aws_apigatewayv2_api.example.execution_arn}/*/*"
 }
-
-# resource "aws_apigatewayv2_api_mapping" "example-dev" {
-#   api_id      = aws_apigatewayv2_api.example.id
-#   domain_name = aws_apigatewayv2_domain_name.example.id
-#   stage       = aws_apigatewayv2_stage.example-dev.id
-# }
